d31e8ce464d01f544cc98ad2180ef286fe911979
PvlDarius <106494736+PvlDarius@users.noreply.github.com>
2023-01-05 01:58:15 +0200

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library;
+
+/**
+ *
+ * @author Darius
+ */
+public class MainApp extends javax.swing.JFrame{
+    public static void main(String[] args) {
+        LogIn login = new LogIn();
+        login.setVisible(true);
+    }
+}
